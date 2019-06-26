82a531b40c77899b8398ac371528af06fdc3a696
Jordan <jtfranklin04203@gmail.com>
2019-06-26 10:12:17 -0400

    experimenting more

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+// addin middleware
+app.set("view engine", "ejs");   //setting up template engine 
+
+app.get('/home', function (req, res) {
+  res.render('home');
 })
  
 app.listen(3000, function(err){
-    if (err){
-        console.log(err)
-        console.log('Server is live on port 3000')
-    }
+    if (err)
+         console.log(err)
+         console.log('Server is live on port 3000')
 })
